a15ae08aa2e2a8016cb130930d8a1ac154cd4102
Chris Bautista <chrisbautista@netflix.com>
2023-05-30 14:56:54 -0700

    asdasd

--- first_steps.ts
+++ first_steps.ts
@@ -1,22 +1,18 @@
-import { serve } from 'https://deno.land/std@0.190.0/http/server.ts';
+import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 
-const handler = async (_request: Request): Promise<Response> => {
-	const resp = await fetch('https://api.github.com/users/denoland', {
-		// The init object here has an headers object containing a
-		// header that indicates what type of response we accept.
-		// We're not specifying the method field since by default
-		// fetch makes a GET request.
-		headers: {
-			accept: 'application/json',
-		},
-	});
+const isEven = (n: number): boolean => n % 2 === 0;
+// deno-lint-ignore require-await
+const handler = async (req: Request): Promise<Response> => {
+  const x = new URLSearchParams(req.url);
+  const input = x.get("input");
+  const test = isEven(input as unknown as number) ? "even" : "odd";
 
-	return new Response(resp.body, {
-		status: resp.status,
-		headers: {
-			'content-type': 'application/json',
-		},
-	});
+  return new Response(test, {
+    status: 200,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
 };
 
 serve(handler);
