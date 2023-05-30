cd3cbf9c1c94e1d839e83231cf55207828f89ad8
Chris Bautista <chrisbautista@netflix.com>
2023-05-30 15:04:03 -0700

    asdasd

--- first_steps.ts
+++ first_steps.ts
@@ -3,7 +3,7 @@ import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 const isEven = (n: number): boolean => n % 2 === 0;
 // deno-lint-ignore require-await
 const handler = async (req: Request): Promise<Response> => {
-  const x = new URLSearchParams(req.url);
+  const x = new URLSearchParams(new URL(req.url).searchParams);
   const input = x.get("input");
   const test = isEven(Number(input)) ? "even" : "odd";
 
